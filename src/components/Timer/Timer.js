33fd7d1837e903a8319c5fefb2faa1e76c8d17ff
Pawel Godula <bolomaster7@gmail.com>
2022-11-29 18:21:13 +0100

    timer changes

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,5 +1,6 @@
-//import styles from './Timer.module.scss'
+import styles from './Timer.module.scss'
 import React, { useState, useEffect } from "react";
+
   
 const Timer = () => {
   const [time, setTime] = useState(0);
@@ -16,16 +17,16 @@ const Timer = () => {
     return () => clearInterval(interval);
   }, [running]);
   return (
-    <div className="Timer">
-      <div className="numbers">
-        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
-        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
-        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+    <div className={styles.timer}>
+      <div className={styles.numbers}>
+        <span className={styles.span}>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span className={styles.span}>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span className={styles.span}>{("0" + ((time / 10) % 100)).slice(-3)}</span>
       </div>
       <div className="buttons">
-        <button onClick={() => setRunning(true)}>Start</button>
-        <button onClick={() => setRunning(false)}>Stop</button>
-        <button onClick={() => setTime(0)}>Reset</button>       
+        <button className={styles.button} onClick={() => setRunning(true)}>Start</button>
+        <button className={styles.button} onClick={() => setRunning(false)}>Stop</button>
+        <button className={styles.button} onClick={() => setTime(0)}>Reset</button>       
       </div>
     </div>
   );
